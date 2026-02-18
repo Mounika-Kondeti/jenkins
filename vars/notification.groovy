f12eaa58023dbc7d5a008f0a060fe156e556556b
mounikakondeti <149581172+Mounika-Kondeti@users.noreply.github.com>
2026-02-18 16:23:47 +0530

    Update notification.groovy

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -11,13 +11,10 @@ def call(String buildStatus = 'STARTED') {
 
   // Override default values based on build status
   if (buildStatus == 'STARTED') {
-    color = 'YELLOW'
     colorCode = '#FFFF00'
   } else if (buildStatus == 'SUCCESS') {
-    color = 'GREEN'
     colorCode = '#00FF00'
   } else {
-    color = 'RED'
     colorCode = '#FF0000'
   }
 
