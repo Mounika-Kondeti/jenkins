b87e8129645e0b5c9215aca85d03c6013a1d9108
mounikakondeti <149581172+Mounika-Kondeti@users.noreply.github.com>
2026-02-19 11:41:28 +0530

    Create checkoutCode.groovy

---
+++ vars/checkoutCode.groovy
@@ -0,0 +1,14 @@
+def call(String branch) {
+    stage('checkout-code') {
+        println "======================================== \n     Cloning the git repo... \n========================================"
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: "*/${branch}"]],
+            extensions: [],
+            userRemoteConfigs: [[
+                credentialsId: 'suryadesy_github',
+                url: 'https://github.com/Desynova/Contido.git'
+            ]]
+        ])
+    }
+}
